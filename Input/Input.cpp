8182b217a42d804c4c67a46598b9863f0dff84d3
jason <jayevo95@gmail.com>
2025-10-03 08:36:22 +0100

    Initial commit

---
+++ Input/Input.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Jason Evans on 03/10/2025.
+//
+
+#include "Input.h"
+
+
---
+++ Input/Input.h
@@ -0,0 +1,13 @@
+//
+// Created by Jason Evans on 03/10/2025.
+//
+
+#ifndef CATALYSTENGINEV2_INPUT_H
+#define CATALYSTENGINEV2_INPUT_H
+
+class Input {
+
+};
+
+
+#endif //CATALYSTENGINEV2_INPUT_H
