840eb15b1d1ef07478d2cbdfe75c085e1b71e508
jason <jayevo95@gmail.com>
2025-10-04 11:23:37 +0100

    Added the flags struct

---
+++ Flags/Flags.h
@@ -0,0 +1,15 @@
+//
+// Created by Jason Evans on 04/10/2025.
+//
+
+#ifndef CATALYSTENGINEV2_FLAGS_H
+#define CATALYSTENGINEV2_FLAGS_H
+
+typedef struct Flags {
+    bool isFacingLeft;
+    bool isRunningSide;
+    bool isJumping;
+    bool isIdle;
+}Flags;
+
+#endif //CATALYSTENGINEV2_FLAGS_H
