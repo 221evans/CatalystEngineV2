8f409a26a676579779beb6ade0769821ea573324
jason <jayevo95@gmail.com>
2025-10-05 11:24:57 +0100

    Added an animation class

---
+++ Animation.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Jason Evans on 05/10/2025.
+//
+
+#include "Animation.h"
+
+Animation::Animation() : totalFrames(0), frameDelay(0), frameCounter(0), frameIndex(0),frameWidth(64) {
+}
+
+void Animation::Animate(float deltaTime, SDL_FRect* srcRect) {
+
+    frameCounter += deltaTime;
+    if (frameCounter >= frameDelay) {
+
+        frameCounter -= frameDelay;
+        frameIndex = (frameIndex + 1) % frameWidth;
+        srcRect->x = static_cast<float>(frameIndex) *  static_cast<float>(frameWidth);
+    }
+}
+
+
+Animation::~Animation() = default;
+
---
+++ Animation.h
@@ -0,0 +1,23 @@
+//
+// Created by Jason Evans on 05/10/2025.
+//
+
+#ifndef CATALYSTENGINEV2_ANIMATION_H
+#define CATALYSTENGINEV2_ANIMATION_H
+#include "SDL3/SDL.h"
+
+class Animation {
+public:
+    Animation();
+    int totalFrames;
+    float frameDelay;
+    float frameCounter;
+    int frameIndex;
+    int frameWidth;
+
+    void Animate(float deltaTime, SDL_FRect* srcRect);
+    ~Animation();
+};
+
+
+#endif //CATALYSTENGINEV2_ANIMATION_H
