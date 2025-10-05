83cc13dfe91bd74838ebd911d75a05704d22e4af
jason <jayevo95@gmail.com>
2025-10-05 12:14:54 +0100

    Added srcRect as pointer in the animate function

--- Animation.cpp
+++ Animation.cpp
@@ -7,14 +7,14 @@
 Animation::Animation() : totalFrames(0), frameDelay(0), frameCounter(0), frameIndex(0),frameWidth(64) {
 }
 
-void Animation::Animate(float deltaTime, SDL_FRect srcRect) {
+void Animation::Animate(float deltaTime, SDL_FRect* srcRect) {
 
     frameCounter += deltaTime;
     if (frameCounter >= frameDelay) {
 
         frameCounter -= frameDelay;
         frameIndex = (frameIndex + 1) % frameWidth;
-        srcRect.x = static_cast<float>(frameIndex) *  static_cast<float>(frameWidth);
+        srcRect->x = static_cast<float>(frameIndex) *  static_cast<float>(frameWidth);
     }
 }
 
